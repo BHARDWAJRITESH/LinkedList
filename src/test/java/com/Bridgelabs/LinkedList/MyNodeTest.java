649a3c04fba9c7e43885c2b81f7468e66b87f3fb
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-10-05 16:38:47 +0530

    Add: Creating simple LinkedList

--- src/test/java/com/Bridgelabs/LinkedList/MyNodeTest.java
+++ src/test/java/com/Bridgelabs/LinkedList/MyNodeTest.java
@@ -1,8 +1,21 @@
 package com.Bridgelabs.LinkedList;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MyNodeTest {
-	public static void main(String args[]) {
-		System.out.println("***** Welcome to the Linked List program ***** ");
+	@Test
+	public void simpleLinkedListTest() {
+		MyNode<Integer> firstNode = new MyNode<>(56);
+		MyNode<Integer> secondNode = new MyNode<>(30);
+		MyNode<Integer> thirdNode = new MyNode<>(70);
 		
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		
+		boolean result = firstNode.getNext().equals(secondNode) &&
+			secondNode.getNext().equals(thirdNode);
+		
+		Assert.assertTrue(result);		
 	}
 }
